f3b07c7730501f581c252460a607b4c8ab76c9e2
Svitlana Saharieva <149310522+Svitlana3513@users.noreply.github.com>
2024-04-03 23:55:45 +0100

    add

--- src/main.js
+++ src/main.js
@@ -12,18 +12,96 @@ import { renderGallery } from './js/render-functions';
 const inputForm = document.querySelector('.form');
 export const imageGallery = document.querySelector('.gallery');
 const imagePreload = document.querySelector('.loader');
+const btnLoadMore = document.querySelector('.btn-load');
+
+let query;
+let currentPage = 1;
+let maxPage = 0;
+const pageSize = 15;
 
 const lightbox = new SimpleLightbox('.gallery a', { 
     captionsData: 'alt',
     captionDelay: 250,
 });
 
-inputForm.addEventListener('submit', async event => {
+function showLoadMore() {
+    btnLoadMore.classList.remove('is-hidden');
+}
+function hideLoadMore() {
+    btnLoadMore.classList.add('is-hidden');
+}
+
+function showLoader() {
+    imagePreload.classList.remove('is-hidden');
+}
+function hideLoader() {
+    imagePreload.classList.add('is-hidden');
+}
+
+
+inputForm.addEventListener('submit', onFormSubmit)
+btnLoadMore.addEventListener('click', onLoadMoreClick)
+
+async function onFormSubmit(event){
     event.preventDefault();
-    const query = event.target.elements.image.value.trim();
-    const data = await getImages(query, 1);
+    query = event.target.elements.image.value.trim();
+    imageGallery.innerHTML = '';
+    currentPage = 1;
+    if(!query){
+        iziToast.error({
+            title: 'Error',
+            position: 'topRight',
+            color: '#EF4040',
+            messageColor: '#FAFAFB',
+            maxWidth: '432px',
+            height: '88',
+            message: 'Please, fill the form!',
+        })
+        return;
+    }
+    try {
+        showLoader();    
+    const data = await getImages(query, currentPage);
+    maxPage = Math.ceil(data.totalHits / pageSize);
     renderGallery(data.hits);
-})
+    lightbox.refresh();       
+    }
+    
+    catch(err) {
+        console.log(error);
+    }
+    hideLoader();
+    checkBtnStatus();
+    event.target.reset();
+}
+
+async function onLoadMoreClick() {
+    currentPage += 1;
+    showLoader();
+    try {
+        const data = await getImages(query, currentPage);
+        renderGallery(data.hits);
+     }
+    catch(err) {
+        console.log(error);
+    }
+    checkBtnStatus();
+    hideLoader();
+}
+
+function checkBtnStatus(){
+    if (currentPage >= maxPage) {
+        hideLoadMore();
+      iziToast.info({
+      message: "We're sorry, but you've reached the end of search results.",
+      position: 'topRight',
+    });
+    } else {
+        showLoadMore();
+}
+}
+
+    
 
 
 
